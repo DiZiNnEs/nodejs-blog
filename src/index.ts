49d484622b1947eef03b95b8cba9f88db328a80e
dizinnes <dizinnes228@gmail.com>
2021-08-03 19:04:46 +0600

    feat(views): Connect html file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express from 'express';
 
+import  { Request, Response } from 'express';
+
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.get('/', (req:Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'templates/index.html'));
+})
+
 app.listen(port, () => {
-  console.log(`Application has been started, ${port}`)
+  console.log(`Application has been started on ${port}`);
 });
