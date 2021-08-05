89ac522d5a5f7e954aa30a7a2570186744f2ba41
dizinnes <dizinnes228@gmail.com>
2021-08-05 14:22:26 +0600

    feat(config): Create interface

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
-export const DATABASE = {
+export interface IDatabase {
+  user: string,
+  host: string,
+  database: string,
+  password: string | number
+  port: string | number
+}
+
+export const DATABASE: IDatabase = {
   user: "postgres",
   host: "db",
   database: "blog",
