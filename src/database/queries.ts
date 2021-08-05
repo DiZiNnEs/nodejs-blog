543b3f2c0a74aa85e8ad55c6b6e52096fc6ee014
dizinnes <dizinnes228@gmail.com>
2021-08-05 18:34:45 +0600

    feat(database): Create queries.ts and add it to index.ts

---
+++ src/database/queries.ts
@@ -0,0 +1,15 @@
+const {Pool} = require('pg');
+
+import { DATABASE } from "../config";
+
+export class Queries {
+  pool: any;
+  constructor() {
+    this.pool = new Pool(DATABASE)
+  }
+
+  async getPosts() {
+    const query = 'SELECT t.* FROM public.blog_posts t';
+    return this.pool.query(query)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import  { Request, Response } from 'express';
 import { main } from "./database/models";
+import { Queries } from "./database/queries";
 
 const path = require('path');
 const app = express();
@@ -10,8 +11,14 @@ const port = 3000;
 
 main()
 
+const queries = new Queries()
+
 app.get('/', (req:Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'templates/index.html'));
+})
+
+app.get('/blogs', async (req: Request, res: Response) => {
+  res.send(await queries.getPosts())
 })
 
 app.listen(port, () => {
